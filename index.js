9f805ea38cbfbef9f6430d2e093145b2404bc6ce
Wendy Du <s7du@ucsd.edu>
2026-01-20 17:29:15 -0800

    mobile changes

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
-
-// --- Render GitHub Stats ---
-const githubData = await fetchGitHubData('wendydu1102'); // Using your GitHub username
-const profileStats = document.querySelector('#profile-stats');
-
-if (profileStats && githubData) {
-  profileStats.innerHTML = `
-        <dl>
-          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-          <dt>Followers:</dt><dd>${githubData.followers}</dd>
-          <dt>Following:</dt><dd>${githubData.following}</dd>
-        </dl>
-    `;
-}
+import { fetchJSON, renderProjects } from './global.js';
 
 // --- Render Latest Projects ---
 const projects = await fetchJSON('./lib/projects.json');
