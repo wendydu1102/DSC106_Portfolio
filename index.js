006430f932014136a1b495a252459e85657d3709
Wendy Du <s7du@ucsd.edu>
2026-01-21 17:34:37 -0800

    updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { fetchJSON, renderProjects } from './global.js';
 
 // --- Render Latest Projects ---
 const projects = await fetchJSON('./lib/projects.json');
-const latestProjects = projects.slice(0, 3);
+const latestProjects = projects.slice(0, 4);
 const projectsContainer = document.querySelector('.projects');
 
 if (latestProjects && projectsContainer) {
